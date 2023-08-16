202ebd3c5cbbc2c424dc007264eeca01d0223a2c
aetinx <sargentcoding@gmail.com>
2023-08-16 21:31:40 +0000

    Aha! This should work!

--- .eleventy.js
+++ .eleventy.js
@@ -1,25 +1,17 @@
 const { DateTime } = require("luxon");
-const fs = require("fs");
-const njk = require("nunjucks");
+const pluginSEO = require("eleventy-plugin-seo");
 
 module.exports = function(eleventyConfig) {
-  
-  // Define the template formats to be processed by Eleventy
-  eleventyConfig.setTemplateFormats(["html", "njk", "md", "css", "js"]);
-  
-  // Copy static assets from the "public" directory to the output directory
+  eleventyConfig.setTemplateFormats(["*"]);
   eleventyConfig.addPassthroughCopy("public");
-  
-  // Handle extensionless files
-  eleventyConfig.setUseGitIgnore(false);
-  eleventyConfig.addPassthroughCopy({"src/*": "/"}); // This will copy extensionless files directly
-  
+
   // Filters let you modify the content https://www.11ty.dev/docs/filters/
   eleventyConfig.addFilter("htmlDateString", dateObj => {
     return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
   });
 
-  // Set the input directory, includes directory, and output directory
+  eleventyConfig.setBrowserSyncConfig({ ghostMode: false });
+
   return {
     dir: {
       input: "src",
